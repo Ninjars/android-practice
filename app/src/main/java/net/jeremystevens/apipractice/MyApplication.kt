b1683663e057112be7d4b30cd38a2cca4c423d69
Jeremy Stevens <jeremy.stevens@bglgroup.co.uk>
2019-04-24 17:24:40 +0100

    added timber

--- app/src/main/java/net/jeremystevens/apipractice/MyApplication.kt
+++ app/src/main/java/net/jeremystevens/apipractice/MyApplication.kt
@@ -3,10 +3,16 @@ package net.jeremystevens.apipractice
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 import net.jeremystevens.apipractice.dagger.DaggerAppComponent
+import timber.log.Timber
 
 class MyApplication : DaggerApplication() {
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.builder().application(this).build()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
     }
 }
