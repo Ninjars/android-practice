6528b3f866d38f6ab11a674ff2f000f5d66bce6b
Jeremy Stevens <jeremy.stevens@bglgroup.co.uk>
2019-07-18 17:25:32 +0100

    upgraded icon colour generation to use better colour theory

--- app/src/main/java/net/jeremystevens/apipractice/features/icongenerator/IconRepository.kt
+++ app/src/main/java/net/jeremystevens/apipractice/features/icongenerator/IconRepository.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import android.util.SparseArray
 import androidx.core.util.contains
 import javax.inject.Inject
+import kotlin.math.roundToInt
 import kotlin.random.Random
 
 class IconRepository @Inject constructor() {
@@ -33,9 +34,13 @@ class IconRepository @Inject constructor() {
         return Bitmap.createBitmap(colors, 0, ICON_WIDTH, ICON_WIDTH, ICON_HEIGHT, Bitmap.Config.ARGB_8888)
     }
 
+    // credit to https://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/ for algorithm
     private fun generateIconColours(random: Random): IntArray {
-        val color1 = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256))
-        val color2 = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256))
+        val primaryHue = random.nextDouble()
+        // offset second hue to ensure colours aren't too similar
+        val secondaryHue = (primaryHue + 0.3 + (random.nextDouble() * 0.4)) % 1
+        val color1 = generateColor(primaryHue, 0.5, 0.95)
+        val color2 = generateColor(secondaryHue, 0.5, 0.95)
 
         val pixels = IntArray(ICON_WIDTH * ICON_HEIGHT)
         for (y in 0 until ICON_HEIGHT) {
@@ -47,5 +52,27 @@ class IconRepository @Inject constructor() {
             }
         }
         return pixels
+    }
+
+    private fun generateColor(hue: Double, sat: Double, value: Double) : Int {
+        val goldenRationConjugate = 0.618033988749895
+        val hueMod = (hue + goldenRationConjugate) % 1
+
+        val hueInt = Math.floor(hueMod * 6).toInt()
+        val factor = hueMod * 6 - hueInt
+
+        val p = value * (1 - sat)
+        val q = value * (1 - factor * sat)
+        val t = value * (1 - (1 - factor) * sat)
+
+        val rgb = when (hueInt) {
+            1 -> arrayOf(q, value, p)
+            2 -> arrayOf(p, value, t)
+            3 -> arrayOf(p, q, value)
+            4 -> arrayOf(t, p, value)
+            5 -> arrayOf(value, p, q)
+            else -> arrayOf(value, t, p)
+        }
+        return Color.argb(255, (rgb[0] * 255).roundToInt(), (rgb[1] * 255).roundToInt(), (rgb[2] * 255).roundToInt())
     }
 }
