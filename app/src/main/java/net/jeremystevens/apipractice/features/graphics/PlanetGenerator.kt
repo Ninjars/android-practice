cdc5bb63c65db430fe56c9cf02e78a9c6bfa884e
Jeremy Stevens <jeremy.stevens@bglgroup.co.uk>
2019-07-19 12:28:52 +0100

    adjustments to handle certain climate conditions for primary colour consistency

--- app/src/main/java/net/jeremystevens/apipractice/features/graphics/PlanetGenerator.kt
+++ app/src/main/java/net/jeremystevens/apipractice/features/graphics/PlanetGenerator.kt
@@ -1,7 +1,6 @@
 package net.jeremystevens.apipractice.features.graphics
 
 import android.graphics.*
-import net.jeremystevens.apipractice.features.graphics.GraphicsUtils.generateColor
 import net.jeremystevens.apipractice.features.swapi.planet.ui.PlanetContract
 import javax.inject.Inject
 import kotlin.random.Random
@@ -43,7 +42,7 @@ class PlanetGenerator @Inject constructor() {
 
         paint.color = Color.HSVToColor(floatArrayOf(0f, 0f, VALUE_FULL))
         paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP)
-        canvas.drawCircle(CENTER + xOffset, CENTER + yOffset, radius + (distance / 2), paint)
+        canvas.drawCircle(CENTER + xOffset, CENTER + yOffset, radius + (distance * 0.5f), paint)
 
         return bitmap
     }
@@ -54,14 +53,15 @@ class PlanetGenerator @Inject constructor() {
         val canvas = Canvas(bitmap)
 
         val random = Random(planetData.hashCode())
-        val radius = random.nextFloat() * 60 + 40
-        val distance = random.nextFloat() * radius * 0.4f + (radius * 0.1f)
-        val angle = random.nextFloat() * Math.PI * 0.7 + Math.PI * 0.65
+        val radius = 40 + random.nextFloat() * 60
+        val distance = radius * 0.2f + random.nextFloat() * radius * 0.5f
+        val angle = Math.PI * 0.7 + random.nextDouble() * Math.PI * 0.6
         val brightnessMask = createBrightnessMask(distance, angle, radius)
 
-        val primaryHue = random.nextDouble()
-        val primeColour = generateColor(primaryHue, 0.5, 0.9)
-        val secondaryHue = (primaryHue + 0.3 + (random.nextDouble() * 0.4)) % 1
+        val primaryHue = generatePrimaryHue(random, planetData.climate)
+        val primarySat = generateSaturation(random, planetData.climate)
+        val primaryVal = generateValue(random, planetData.climate)
+        val primeColour = Color.HSVToColor(floatArrayOf(primaryHue * 360, primarySat, primaryVal))
 
         val paint = Paint().apply {
             color = primeColour
@@ -75,5 +75,35 @@ class PlanetGenerator @Inject constructor() {
         bitmap.recycle()
         brightnessMask.recycle()
         return scaledBitmap
+    }
+
+    private fun generatePrimaryHue(random: Random, climate: String): Float {
+        val (offset, variance) = when (climate) {
+            "temperate" -> Pair(0.33, 0.15)
+            "arid" -> Pair(0.1, 0.06)
+            "polluted" -> Pair(0.8, 0.15)
+            else -> Pair(random.nextDouble(), 0.0)
+        }
+        return valueWithVariance(random, offset, variance) % 1
+    }
+
+    private fun generateSaturation(random: Random, climate: String): Float {
+        return when (climate) {
+            "polluted" -> valueWithVariance(random, 0.6, 0.1)
+            else -> valueWithVariance(random, 0.9, 0.05)
+        }
+    }
+
+    private fun generateValue(random: Random, climate: String): Float {
+        return when (climate) {
+            "temperate" -> valueWithVariance(random, 0.6, 0.1)
+            "arid" -> valueWithVariance(random, 0.8, 0.1)
+            "polluted" -> valueWithVariance(random, 0.4, 0.1)
+            else -> valueWithVariance(random, 0.5, 0.2)
+        }
+    }
+
+    private fun valueWithVariance(random: Random, value: Double, variance: Double): Float {
+        return ((value - variance) + variance * 2 * random.nextDouble()).toFloat()
     }
 }
